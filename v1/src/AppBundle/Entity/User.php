daaa8dbca68488fa47f211eb8adb21297f4e9512
GeLe1974 <74gl74@gmail.com>
2016-09-22 11:23:34 +0200

    FOSUser added

---
+++ v1/src/AppBundle/Entity/User.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Geert
+ * Date: 22/09/16
+ * Time: 09:38
+ */
+
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
